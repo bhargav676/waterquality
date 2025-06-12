ac3377b39bbe714097621860ca68cce593fce759
Bhargav <bhargavk1290@gmail.com>
2025-06-12 12:52:57 +0530

    socket

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['https://waterquality-five.vercel.app/'],
+    origin: ['https://waterquality-five.vercel.app'],
     methods: ['GET', 'POST'],
   }, 
 });
